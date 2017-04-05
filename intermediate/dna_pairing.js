bd22922733698a07d5e4a05a9ec4e907aa430210
Dragan Roganović <unrealityxi@gmail.com>
2017-04-05 13:23:06 +0200

    DNA pairing algorithm

---
+++ intermediate/dna_pairing.js
@@ -0,0 +1,23 @@
+// Creates base DNA pairs based on string. 
+
+function pairElement(str) {
+
+  // Lookup table with base pair maping
+  const lookup = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  }
+
+  // Split str to array
+  // map base pairs into 2 member subarray
+  // use lookup table to pair them
+  let paired = str.split("").map((letter) => {
+    return [letter, lookup[letter]];
+  });
+
+  return paired;
+}
+
+pairElement("GCG");
