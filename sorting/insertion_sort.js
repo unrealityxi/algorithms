c78d2163fd5a6150fb6d4d914a77f34ebc4ca4ca
Dragan Roganović <unrealityxi@gmail.com>
2017-05-09 23:20:30 +0200

    Implementation of insertion sort

--- sorting/insertion_sort.js
+++ sorting/insertion_sort.js
@@ -2,33 +2,21 @@ let arr = [51, 12, -12, 154, 152351, 1111, 12, 75347, 2356, 6235, 53];
 
 function insertion_sort(arr){
 
+  let len = arr.length;
   // traverse the array
   // assume that first item in array is already sorted
-  for (let i = 1; i < arr.length; i++){
+  for (let i = 1; i < len; i++){
     
     // find index at which current entry is smaller then entry
     // to its left, and bigger from preceeding one (j)
-    for (var j = 0; j < i; j++){
-      if (arr[i] < arr[j]){
-        break;
-      }
-    }
-
-    //This is an alternative to the above step: 
-    // let j = i - 1; 
-    // while (arr[i] < arr[j]){
-    //   j--
-    // }
-    // j += 1;
-
-
     // shift members of array one place to the right
     // from current position down to and including 
     // entry at j index 
     let forMoving = arr[i];
-
-    for (let k = i; k > j; k--){
-      arr[k] = arr[k - 1];
+    let j = i; 
+    while (j > 0 && (arr[j - 1] > forMoving)){
+      arr[j] = arr[j -1];
+      j--
     }
 
     // finaly, place current value at J index. 
